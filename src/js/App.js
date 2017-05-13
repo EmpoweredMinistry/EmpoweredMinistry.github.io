cde3fe4ae98688191b9ae71db8dc6066484f22c4
mannycolon <colonmanuel7@gmail.com>
2017-05-13 13:18:50 -0400

    moved header our of app.js. removed carousel. and added the Cards component which holds the mission and vision cards.

--- src/js/App.js
+++ src/js/App.js
@@ -1,41 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // assets
-import logo from './assets/logo.png';
-import '../css/App.css';
+import '../css/App.css'
 // components
-import PhotosCarousel from './PhotosCarousel';
+import Header from './Header'
+import Cards from './Cards'
 
 class App extends Component {
   constructor(props){
-    super(props);
-    this.state = {
-      index: 0,
-      direction: null
-    };
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-
-  handleSelect(selectedIndex, e) {
-    this.setState({
-      index: selectedIndex,
-      direction: e.direction
-    });
+    super(props)
+    this.state = {}
   }
 
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <Header />
+        <div className="App-body">
+          <Cards />
         </div>
-        <PhotosCarousel
-          index={this.state.index}
-          direction={this.state.direction}
-          handleSelect={this.handleSelect}
-        />
       </div>
     );
   }
 }
 
-export default App;
+export default App
